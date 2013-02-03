ffac492a247a7182ab21f64fc16fbee7664d303d
Menno de Gier <mrdegier@gmail.com>
2013-02-03 17:30:54 +0100

    chord and metronome functions

--- runtime/flim.lua
+++ runtime/flim.lua
@@ -36,3 +36,23 @@ end
 function pick(a, b, condition)
   if condition then return a else return b end
 end
+
+function chord(time, pitches, velocity, duration, channel)
+  for _, p in ipairs(pitches) do
+    note(time, p, velocity, duration, channel)
+  end
+end
+
+function make_metro(tempo)
+  local tempo = tempo
+  local time = now()
+  local g_tempo = 60.0 / tempo
+
+  return function(str, ...)
+    if type(str) == "number" then
+      return str * g_tempo * 1000
+    elseif str == 'beat' then
+      return math.ceil(((now() - time) / (1000 * g_tempo)))
+    end
+  end
+end
